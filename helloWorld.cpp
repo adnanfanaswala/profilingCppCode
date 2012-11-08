b7c4cd37cefdc1825ecfa900864e2af8f2756963
Adnan Fanaswala <adnan.fanaswala@gmail.com>
2012-11-08 15:16:05 -0800

    Simple HelloWorld Program added

---
+++ helloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+#-------------------------------------------------------------------------------
+# Author: Adnan Fanaswala
+# Created: 14th August 2012
+# Simple getting started C++ Hello World program
+#-------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World !" << endl;
+    system("PAUSE");
+    return(0);
+}
